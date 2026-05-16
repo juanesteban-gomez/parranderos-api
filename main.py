ce03c3eaa29056cd8614d2f6694f74368b248164
juanesteban-gomez <juanesteban.cuenta@gmail.com>
2026-05-16 18:06:04 -0500

    implemantacion ult 2 ptos

--- main.py
+++ main.py
@@ -43,7 +43,17 @@ def post_comentario(bar_id: int, datos: dict):
 
 # TODO: implementar GET /bares/{bar_id}/eventos
 # Debe retornar todos los eventos del bar desde la colección 'eventos'
+@app.get('/bares/{bar_id}/eventos')
+def get_eventos (bar_id: int):
+    eventos= list(db["eventos"].find({"bar_id":bar_id},{"_id":0}))
+    return eventos
 
 # TODO: implementar POST /bares/{bar_id}/eventos  
 # Debe insertar el evento en la colección 'eventos'
 # Recuerde agregar bar_id y fecha_creacion al documento antes de insertar
+@app.post("'/bares/{bar_id}/eventos")
+def post_eventos(bar_id: int, datos: dict):
+    datos['bar_id'] = bar_id
+    datos['fecha']  = datetime.now().isoformat()
+    db["comentarios"].insert_one(datos)
+    return {"mensaje":"Evento creado"}
